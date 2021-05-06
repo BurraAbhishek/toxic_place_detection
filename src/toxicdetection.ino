652cb7c6587e42ac3bd772583fefb1ef7db49f73
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-05-06 21:54:12 +0530

    Assign different PIN to buzzer

--- src/toxicdetection.ino
+++ src/toxicdetection.ino
@@ -1,9 +1,17 @@
 void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(6, OUTPUT);
+    // This code is based on the circuit diagram described in the README section.
+    pinMode(2, OUTPUT); // Green Light
+    pinMode(4, OUTPUT); // Yellow Light
+    pinMode(6, OUTPUT); // Red Light
+    pinMode(8, OUTPUT); // Buzzer
+    pinMode(A0, INPUT); // Gas sensor 1
+    // pinMode(A1, INPUT); // Gas sensor 2
+    // pinMode(A2, INPUT); // Gas sensor 3
+    // pinMode(A3, INPUT); // Gas sensor 4
+    // pinMode(A4, INPUT); // Gas sensor 5
+    // pinMode(A5, INPUT); // Gas sensor 6
 }
 
 void loop() {
-  
+    
 }
