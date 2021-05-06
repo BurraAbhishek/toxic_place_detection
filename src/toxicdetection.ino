85984644950cad54a3ada7509ee308da7b6bbf96
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-05-06 22:27:15 +0530

    Add basic functions

--- src/toxicdetection.ino
+++ src/toxicdetection.ino
@@ -13,5 +13,45 @@ void setup() {
 }
 
 void loop() {
-    
+    // Interfacing with MQ135 to begin with
+    int air_quality = analogRead(A0);
+    if(air_quality < 150) {
+        // Safe - Green
+        safezone();
+    } else if(air_quality > 150 && air_quality < 300) {
+        // Slightly hazardous - indicates severe hazards ahead as warning - Yellow
+        warnzone();
+    } else if(air_quality > 300) {
+        // Extremely hazardous - Must avoid - Red
+        dangerzone();
+    }
+    delay(1000); // The time interval between two readings is 1000 milliseconds (1 second).
+}
+
+// Whatever should happen in case of safe environment - No hazards ahead.
+void safezone() {
+    digitalWrite(2, HIGH); // Green light glows
+    digitalWrite(4, LOW); // Yellow light does not glow
+    digitalWrite(6, LOW); // Red light does not glow
+    digitalWrite(8, LOW); // Switch off the buzzer
+}
+
+// Whatever should happen in case of approach towards hazardous environment - Warn the user of the dangers ahead
+void warnzone() {
+    digitalWrite(2, LOW); // Green light does not glow
+    digitalWrite(4, HIGH); // Yellow light glows
+    digitalWrite(6, LOW); // Red light does not glow
+    // Buzzer beeps at regular intervals
+    digitalWrite(8, HIGH);
+    delay(500);
+    digitalWrite(8, LOW);
+    delay(500);
+}
+
+// Whatever should happen in a hazardous environment - Warn the user to vacate immediately
+void dangerzone() {
+    digitalWrite(2, LOW); // Green light does not glow
+    digitalWrite(4, LOW); // Yellow light does not glow
+    digitalWrite(6, HIGH); // Red light glows
+    digitalWrite(8, HIGH); // The buzzer emits continuous sound until the environment is safe
 }
